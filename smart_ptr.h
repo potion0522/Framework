5dd645892cace9be68250f898fee1dcd6fcbe9c1
tika <kt.potion0522w@gmail.com>
2019-09-25 14:58:33 +0900

    構造体のdefineの追加

--- smart_ptr.h
+++ smart_ptr.h
@@ -14,3 +14,13 @@ std::shared_ptr< class > Çè»ó™Ç∑ÇÈÇΩÇﬂÇ…égópÇ∑ÇÈ
 	typedef std::unique_ptr< NAME > NAME##UniquePtr;\
 
 #endif
+
+#ifndef STRUCT_PTR
+#define STRUCT_PTR( NAME )\
+	struct NAME;\
+	typedef std::shared_ptr< NAME > NAME##Ptr;\
+	typedef std::shared_ptr< const NAME > NAME##ConstPtr;\
+	typedef std::weak_ptr< NAME > NAME##WeakPtr;\
+	typedef std::unique_ptr< NAME > NAME##UniquePtr;\
+
+#endif
